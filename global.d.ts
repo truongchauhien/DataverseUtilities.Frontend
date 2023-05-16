b65e2ce4949d6d00876793e96ebdebe513aa7ee0
Truong Chau Hien <truongchauhien@gmail.com>
2023-05-16 16:52:54 +0700

    Initial commit

---
+++ global.d.ts
@@ -0,0 +1,8 @@
+declare module '*.scss' {
+    const content: any;
+    export default content;
+}
+
+declare const USE_HTTPS: boolean;
+declare const API_HOST: string;
+declare const API_PORT: number;
